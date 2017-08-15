bcc75fd110417cc0ec0d1820910dc575758cf843
ECManning <31044186+ECManning@users.noreply.github.com>
2017-08-15 14:08:07 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,35 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int variableName= 5;//this is the first variable
+		char character = 'c';//this is the first character
+		String hello = "Hello, world! ";
+		
+		int x = 6;//be careful of integer division
+		int y = 10;//use "(double)" to typecast and get the correct answer
+		
+		//System.out.println(x<y || x!=y);
+		int n = 15;
+		int answer = 1;
+		for(int i=1; i < 5; i=i+1);
+		{
+		answer = i*answer;
+		}
+		System.out.println(answer);
+		}
+		//while(true)
+		{
+		
+			//System.out.println(x);
+			//x = x+1;
+		}
+		int m = 5
+		if(m == 0)
+		{
+			System.out.println((double)1/m);
+		}
+}}
